cf06c348982c0a9a125420b8285cf35d0c60e3ab
Ben Arnold <B.Arnold@LiveLinkTechnology.net>
2020-11-12 20:56:40 +0000

    Up: Bash vars are uppercase

--- gcloud.bash
+++ gcloud.bash
@@ -8,9 +8,9 @@ function init_tooling {
 
 function init_gcloud_auth {
   echo ""; echo "·  Authenticating with Google Cloud..."
-  auth_command="gcloud auth login"
-  list_command="gcloud auth list 2> /dev/null | grep \*"
-  docker-compose run --rm app sh -c "($list_command) || (($auth_command) && ($list_command))"
+  AUTH_COMMAND="gcloud auth login"
+  LIST_COMMAND="gcloud auth list 2> /dev/null | grep \*"
+  docker-compose run --rm app sh -c "($LIST_COMMAND) || (($AUTH_COMMAND) && ($LIST_COMMAND))"
 }
 
 function init_gcloud_project {
@@ -18,10 +18,10 @@ function init_gcloud_project {
   docker-compose run --rm app gcloud projects list
 
   echo -n "Project ID: "
-  read project_id
+  read PROJECT_ID
 
-  echo "" ; echo "·  Setting your active project to $project_id ..."
-  docker-compose run --rm app gcloud config set project $project_id
+  echo "" ; echo "·  Setting your active project to $PROJECT_ID ..."
+  docker-compose run --rm app gcloud config set project $PROJECT_ID
 }
 
 function init_k8s_cluster {
@@ -29,12 +29,12 @@ function init_k8s_cluster {
   docker-compose run --rm app gcloud container clusters list
 
   echo -n "Cluster name: "
-  read cluster_name
+  read CLUSTER_NAME
   echo -n "Cluster location: "
-  read cluster_region
+  read CLUSTER_REGION
 
-  echo "" ; echo "·  Connecting to the $cluster_name cluster..."
-  docker-compose run --rm app gcloud container clusters get-credentials $cluster_name --region $cluster_region --project $project_id
+  echo "" ; echo "·  Connecting to the $CLUSTER_NAME cluster..."
+  docker-compose run --rm app gcloud container clusters get-credentials $CLUSTER_NAME --region $CLUSTER_REGION --project $PROJECT_ID
 }
 
 function init_k8s_namespace {
@@ -42,22 +42,22 @@ function init_k8s_namespace {
   docker-compose run --rm app kubectl get namespaces
 
   echo -n "Namespace: "
-  read namespace_name
+  read NAMESPACE_NAME
 }
 
 function init_k8s_pods {
-  echo "" ; echo "·  Getting pods from $namespace_name ..."
-  docker-compose run --rm app kubectl get pods -n $namespace_name | grep web-kiosk-foreground
+  echo "" ; echo "·  Getting pods from $NAMESPACE_NAME ..."
+  docker-compose run --rm app kubectl get pods -n $NAMESPACE_NAME | grep web-kiosk-foreground
 
   echo -n "Pod ID: "
-  read pod_id
+  read POD_ID
 
-  echo "" ; echo "·  Connecting to pod: $pod_id ..."
-  docker-compose run --rm app sh -c "kubectl exec -it $pod_id -n $namespace_name -c puma bash"
+  echo "" ; echo "·  Connecting to pod: $POD_ID ..."
+  docker-compose run --rm app sh -c "kubectl exec -it $POD_ID -n $NAMESPACE_NAME -c puma bash"
 }
 
-init_tooling
-init_gcloud_auth
+# init_tooling
+# init_gcloud_auth
 init_gcloud_project
 init_k8s_cluster
 init_k8s_namespace
