be7dca6303552237f638a9cd3f899084d818801a
Ben Arnold <B.Arnold@LiveLinkTechnology.net>
2020-11-12 21:25:29 +0000

    Add: write/read config in a YAML file, like it's still cool

--- gcloud.rb
+++ gcloud.rb
@@ -1,5 +1,27 @@
 #!/usr/bin/env ruby
 
+require 'yaml'
+
+CONFIG_FILE = './config.yml'
+File.exists?(CONFIG_FILE) || File.write(CONFIG_FILE, YAML.dump({}))
+
+def config
+  YAML.load(File.read(CONFIG_FILE))
+end
+def config!(data)
+  File.write(CONFIG_FILE, YAML.dump(data))
+  data
+end
+def get(key)
+  config[key]
+end
+def set(key, value)
+  data = config
+  data[key] = value
+  config!(data)
+  value
+end
+
 def init_tooling
   puts "\n·  (Re-)Creating containers..."
   system(%Q{ docker-compose down 2> /dev/null }) || exit(1)
