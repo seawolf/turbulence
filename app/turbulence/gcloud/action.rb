d6f3d6e14339463c509c724f211c31e91bd818a1
Ben Arnold <B.Arnold@LiveLinkTechnology.net>
2021-07-20 16:58:47 +0100

    Up: tidy Action to private-ify stuff

--- app/turbulence/gcloud/action.rb
+++ app/turbulence/gcloud/action.rb
@@ -5,14 +5,18 @@ module Turbulence
     # Item in the menu system
     class Action
       def initialize
-        choices = Actions::LIST
-                  .map { |action| Action.new(action::ID, action::NAME, action) }
-                  .map(&:to_choice)
-
         action = Menu.auto_select('Select your desired action:', choices, per_page: choices.length)
         Config.set(:action, action.id)
 
         action.class_name.new
+      end
+
+      private
+
+      def choices
+        Actions::LIST
+          .map { |action| Action.new(action::ID, action::NAME, action) }
+          .map(&:to_choice)
       end
 
       Action = Struct.new(:id, :name, :class_name) do
